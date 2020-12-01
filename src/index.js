5997bd1e5f321179e923ce71cff99e2704b16271
shaik jamaluddin <jamaluddinshaik6@example.com>
2020-12-01 18:41:29 +0530

    pagination added 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { ThemeProvider } from '@material-ui/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+// import { ConnectedRouter } from 'connected-react-router';
+import { Provider } from 'react-redux';
+import history from './store/history';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import configureStore from './store/Store';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// const theme = createMuiTheme(customTheme);
+const initialState = {};
+const store = configureStore(initialState, history);
+
+const Root = () => (
+    <Provider store={store}>
+            <ThemeProvider>
+                <CssBaseline />
+                <App />
+            </ThemeProvider>
+    </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// eslint-disable-next-line no-undef
+ReactDOM.render(<Root />, document.getElementById('root'));
